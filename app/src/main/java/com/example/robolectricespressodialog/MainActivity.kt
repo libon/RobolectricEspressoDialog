8dfaabddaa24837dee4a17f8d2e9754f6b44d416
Carmen Alvarez <calvarez@libon.com>
2019-07-03 10:08:40 +0200

    Replace "Hello world" TextView with a Button that, when clicked, shows a dialog

--- app/src/main/java/com/example/robolectricespressodialog/MainActivity.kt
+++ app/src/main/java/com/example/robolectricespressodialog/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.robolectricespressodialog
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.my_button).setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.dialog_title)
+            .setMessage(R.string.dialog_message)
+            .setPositiveButton(android.R.string.ok, object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface, which: Int) {
+                    dialog.dismiss()
+                }
+            })
+            .show()
     }
 }
